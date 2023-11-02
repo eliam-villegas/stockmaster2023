f373bbd89c3f88d85fbae350109411d2856bae06
Eliam Villegas <102682631+eliam-villegas@users.noreply.github.com>
2023-11-02 12:22:22 -0300

    Adición de Driver JDBC de PostgreSQL, intento de conexión a la BD sushi hosteado en el servidor ULS.
    Resta esperar que vuelvan a encender los servidores ya que no conecta exitosamente.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.sql.ConnectionPoolDataSource;
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static String driver = "org.postgresql.Driver";
+    static String dbname = "sushi";
+    static String url = "jdbc:postgresql://10.4.3.195:5432/" + dbname;
+    static String username = "eliam.villegas";
+    static String password = "29805641";
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection conData;
+        Statement stmData;
+        ResultSet rsData;
+        Class.forName(driver);
+        conData = DriverManager.getConnection(url,username,password);
+        stmData = conData.createStatement();
+        try{
+            stmData.execute("SELECT * FROM empleado");
+        }
+        catch (Exception e){
+            System.out.println("No se pudo recuperar la tabla");
+        }
+
     }
+
 }
