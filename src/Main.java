c8ba19285e3f58e44d47cfcdc7ba24db7bacce61
Eliam Villegas <102682631+eliam-villegas@users.noreply.github.com>
2023-11-07 19:16:18 -0300

    Comunicacion base de datos de la U

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-//    static String driver = "org.postgresql.Driver";
-//    static String dbname = "sushi";
-//    static String url = "jdbc:postgresql://10.4.3.195:5432/" + dbname;
-//    static String username = "eliam.villegas";
-//    static String password = "29805641";
     static String driver = "org.postgresql.Driver";
-    static String dbname = "sushi_prueba";
-    static String url = "jdbc:postgresql://localhost:5432/" + dbname;
-    static String username = "postgres";
-    static String password = "29805";
+    static String dbname = "sushi";
+    static String url = "jdbc:postgresql://10.4.3.195:5432/" + dbname;
+    static String username = "sushi";
+    static String password = "stKim72";
+//    static String driver = "org.postgresql.Driver";
+//    static String dbname = "sushi_prueba";
+//    static String url = "jdbc:postgresql://localhost:5432/" + dbname;
+//    static String username = "postgres";
+//    static String password = "29805";
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Connection conData;
         Statement stmData;
