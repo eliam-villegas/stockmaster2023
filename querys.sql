4c4f83bff584a0e0b2277951b3879a9f77b5039d
Eliam Villegas <102682631+eliam-villegas@users.noreply.github.com>
2023-11-08 09:39:30 -0300

    Update querys.sql
    update xd

--- querys.sql
+++ querys.sql
@@ -9,15 +9,15 @@ GROUP BY p.tipo
 -- Este query resulta una tabla que muestra el tipo de producto y la cantidad total vendida para cada categoria de producto
 
 -- Necesidad de informacion:
--- Queremos saber el total de ventas realizadas por cada empleado (campo 'rut_empleado' 
--- en la tabla 'registro_de_venta') durante un período específico de tiempo.
+-- Queremos saber el total de ordenes de venta realizadas por cada empleado (campo 'rut_empleado' 
+-- en la tabla 'orden_de_compra') durante un período específico de tiempo.
 
-SELECT rdv.rut_empleado, e.nombre AS nombre_empleado, COUNT(rdv.id_venta) AS total_ventas
-FROM public.registro_de_venta rdv
-INNER JOIN public.empleado e ON rdv.rut_empleado = e.rut_empleado
-WHERE rdv.fecha_de_pago BETWEEN '2023-01-01' AND '2023-12-31'
-GROUP BY rdv.rut_empleado, e.nombre
+SELECT odc.rut_empleado, e.nombre AS nombre_empleado, COUNT(odc.id_orden) AS total_ventas
+FROM public.orden_de_compra odc
+INNER JOIN public.empleado e ON odc.rut_empleado = e.rut_empleado
+WHERE odc.fecha BETWEEN '2023-01-01' AND '2023-12-31'
+GROUP BY odc.rut_empleado, e.nombre
 ORDER BY total_ventas DESC;
 
--- Con este query obtenemos una tabla que muestra el rut del empleado, ademas de su nombre y las ventas realizadas por
--- cada uno de los empleados durante un periodo de tiempo especificado, ordenado de mayor a menor cantidad de ventas
+-- Con este query obtenemos una tabla que muestra el rut del empleado, ademas de su nombre y las ordenes de compra realizadas por
+-- cada uno de los empleados durante un periodo de tiempo especificado, ordenado de mayor a menor cantidad de ordenes
