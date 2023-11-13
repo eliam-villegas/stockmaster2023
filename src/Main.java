4fdefd72861dabdb0f80e35cc93aadc3d3baf9dd
danielestebanRA <102681651+danielestebanRA@users.noreply.github.com>
2023-11-13 16:04:34 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import javax.swing.SwingUtilities;
 
-public class App {
+public class main {
     public static void main(String[] args) throws Exception {
         SwingUtilities.invokeLater(() -> {
             Login loginFrame = new Login();
