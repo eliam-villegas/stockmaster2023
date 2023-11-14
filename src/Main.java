0ef6ef08cb81a6cf310b4a8e8bd0675f2e7044dc
danielestebanRA <102681651+danielestebanRA@users.noreply.github.com>
2023-11-14 17:21:54 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.SwingUtilities;
 public class Main {
     public static void main(String[] args) throws Exception {
         SwingUtilities.invokeLater(() -> {
-            Login loginFrame = new Login();
+           // Login loginFrame = new Login();
            // loginFrame.setVisible(true);
             //loginFrame.setResizable(false);
             Inventory InvFrame = new Inventory();
